dec8ff598be389108d451614a13f1c00d03bc572
tintin-lab <psupriti16@gmail.com>
2026-07-08 23:45:17 +0900

    Update foodguardian_app.py

--- foodguardian_app.py
+++ foodguardian_app.py
@@ -1,25 +1,42 @@
 import streamlit as st
+import easyocr
 
 st.title("FoodGuardian 🥗")
 
-st.write(
-    "Upload your snack nutrition and ingredient label images"
+
+age_group = st.selectbox(
+    "Select age group",
+    [
+        "Child (5-12 years)",
+        "Adolescent (13-18 years)",
+        "Adult (19-60 years)",
+        "Elderly (>60 years)"
+    ]
 )
 
-uploaded_files = st.file_uploader(
-    "Upload images",
-    type=["jpg", "jpeg", "png"],
-    accept_multiple_files=True
+
+body_weight = st.number_input(
+    "Body weight (kg)",
+    min_value=5,
+    max_value=150,
+    value=50
 )
 
 
-if uploaded_files:
+packet_size = st.number_input(
+    "Packet consumed (grams)",
+    min_value=1,
+    max_value=500,
+    value=30
+)
 
-    st.success("Images uploaded successfully!")
 
-    for file in uploaded_files:
-
-        st.image(
-            file,
-            caption=file.name
-        )
+frequency = st.selectbox(
+    "Consumption frequency",
+    [
+        "Daily",
+        "3-5 times/week",
+        "1-2 times/week",
+        "Occasionally"
+    ]
+)
