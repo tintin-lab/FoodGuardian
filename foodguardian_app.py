9ab911e170ead8362a9e89263974a7a7d6ca2758
tintin-lab <psupriti16@gmail.com>
2026-07-08 23:31:40 +0900

    Update foodguardian_app.py

--- foodguardian_app.py
+++ foodguardian_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 
-st.title("FoodGuardian")
+st.title("FoodGuardian 🥗")
 
-st.write("My food safety analysis tool is working!")
+st.write(
+    "Upload your snack nutrition and ingredient label images"
+)
+
+uploaded_files = st.file_uploader(
+    "Upload images",
+    type=["jpg", "jpeg", "png"],
+    accept_multiple_files=True
+)
+
+
+if uploaded_files:
+
+    st.success("Images uploaded successfully!")
+
+    for file in uploaded_files:
+
+        st.image(
+            file,
+            caption=file.name
+        )
